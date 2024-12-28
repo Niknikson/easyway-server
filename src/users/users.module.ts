0a9f6be21df9b7f36ba292d931a830fd43c45356
Vlad777mykola <vlad777mykola@gmail.com>
2024-12-28 14:33:01 +0200

    chore(set-up): project

---
+++ src/users/users.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersResolver } from './users.resolver';
+import { UsersRepository } from './users.repository';
+import { DatabaseModule } from 'src/common/database/database.module';
+import { User, UserSchema } from './entities/user.entity';
+import { UsersController } from './users.controller';
+import { S3Module } from 'src/common/s3/s3.module';
+
+@Module({
+  imports: [
+    S3Module,
+    DatabaseModule.forFeature([{name: User.name, schema: UserSchema}]),
+  ],
+  providers: [UsersResolver, UsersService, UsersRepository],
+  exports: [UsersService],
+  controllers: [UsersController]
+})
+export class UsersModule {}
